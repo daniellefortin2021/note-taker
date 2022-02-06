4e18eb357ee9a4233ec4a4ad8f6040fab8445b2c
Danielle Fortin <fortind6@gmail.com>
2022-02-06 16:44:16 -0500

    added routes

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
+
+// express app
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+// set up route files = api routes
+const apiRoutes = require('./routes/apiRoutes');
+
+// get notes from api/notes
+// add new note to api file (JSON db file)
+// add new id to note
 
 
+
+// sets up the server
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+    console.log(`API server now on port ${PORT}`);
   });
