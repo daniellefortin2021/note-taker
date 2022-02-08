49eeb6789f72847a1620694c10fdfdab35f9b803
Danielle Fortin <fortind6@gmail.com>
2022-02-07 20:44:16 -0500

    POST set up

--- server.js
+++ server.js
@@ -4,19 +4,30 @@ const fs = require('fs');
 
 // express app
 const app = express();
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
+
 const PORT = process.env.PORT || 3001;
 
 // set up route files = api routes
-const apiRoutes = require('./routes/apiRoutes');
+const notes = require('./routes/notes');
 
 // get notes from api/notes
 // add new note to api file (JSON db file)
 // add new id to note
 
 
-//conected to apiRoutes doc
-app.get('/api/apiRoutes', (req, res) => {
-    res.json(apiRoutes);
+//conected to apiNotes doc
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+})
+
+//post to notes doc
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 })
 
 // sets up the server
