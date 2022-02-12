2cdc205d23e590fbf32906e2c1e0f1dc21f558b4
Danielle Fortin <fortind6@gmail.com>
2022-02-12 13:55:17 -0500

    working now

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const fs = require('fs');
 
+const path = require('path');
+
 //creat unique id
 const { v4: uuidv4 } = require('uuid');
 
@@ -9,23 +11,30 @@ const { notes } = require('../../data/notes.json');
 //conected to notes array
 router.get('/api/notes', (req, res) => {
     let results = notes;
-    console.log(req.query)
+    // console.log(results);
     res.json(results);
 });
 
 //post to notes file then return new note to client
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     // res.json(req.body);
     const newNote = req.body;
     newNote.id = uuidv4();
 
-    //read notes file
-
+    // read file
+    let data = notes;
+    console.log(data);
+    
     //add to notes file
+    data.push(newNote);
 
-    //write file
+    // write file
+    fs.writeFile(path.join(__dirname,'../../data/notes.json'), JSON.stringify(data), (err) => {
+        if (err) throw err;
+    });
 
     // send response
+    res.json(data);
 })
 
 // *BONUS* delete note from array 
