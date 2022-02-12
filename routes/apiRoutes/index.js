f59a922a0a767ca344a26a525fbea42f97988d14
Danielle Fortin <fortind6@gmail.com>
2022-02-12 11:23:42 -0500

    Add Heroku

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
-const { notes } = require('../../data/notes.json');
+const { AsyncResource } = require('async_hooks');
+const fs = require('fs');
+// const { notes } = require('../../data/notes.json');
 
 //conected to notes array
-router.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
+router.get('/notes', (req, res) => {
+    fs.readFile('../../data/notes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            const parsedNotes = JSON.parse(data);
+            res.json(parsedNotes);
+                }
+    })
+    
+    // let results = notes;
+    // console.log(req.query)
+    // res.json(results);
 });
 
 //post to notes array
---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const router = require('express').Router();
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../index.html'));
+});
+
+module.exports = router;
