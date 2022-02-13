a8b6f35d5c31cbd085cf308258db5753b657edda
Danielle Fortin <fortind6@gmail.com>
2022-02-13 12:34:06 -0500

    fixed routes for heroku and debugged

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 //creat unique id
 const { v4: uuidv4 } = require('uuid');
 
-const { notes } = require('../../data/notes.json');
+// const { notes } = require('../../data/notes.json');
 
 //conected to notes array
 router.get('/notes', (req, res) => {
@@ -15,41 +15,38 @@ router.get('/notes', (req, res) => {
         // let results = notes;
         // console.log(results);
         res.json(JSON.parse(data));
-
-        console.log(JSON.parse(data));
     })
 });
 
-// //post to notes file then return new note to client
-// router.post('/notes', (req, res) => {
-//     // res.json(req.body);
-//     const newNote = req.body;
-//     newNote.id = uuidv4();
+//post to notes file then return new note to client
+router.post('/notes', (req, res) => {
+    // res.json(req.body);
+    const newNote = req.body;
+    newNote.id = uuidv4();
 
-//     // const data = notes;
+    // read file
+    // let data = notes;
+    let notesArray = fs.readFileSync(path.join(__dirname,'../../data/notes.json'), (err, data) => { 
+        res.json(JSON.parse(data));
+    });
 
-//     // read file
-//     fs.readFile(path.join(__dirname,'../../data/notes.json'), (err, data) => {
-//         res.json(JSON.parse(data));
+    let notes = JSON.parse(notesArray);
+    
+    //add to notes file
+    notes.push(newNote);
 
-//         console.log(JSON.parse(data));
-//     })
-          
-//     //add to notes file
-//     data.push(newNote);
+    // write file
+    fs.writeFile(path.join(__dirname,'../../data/notes.json'), JSON.stringify(notes), (err) => {
+        if (err) throw err;
+    });
 
-//     // write file
-//     fs.writeFile(path.join(__dirname,'../../data/notes.json'), JSON.stringify(data), (err) => {
-//         if (err) throw err;
-//     });
-
-//     // send response
-//     res.json(data);
-// })
+    // send response
+    res.json(notes);
+})
 
 // *BONUS* delete note from array 
-// router.delete('/notes', (req,res)=> {
-//     res.json(req.body);
-// })
+router.delete('/notes', (req,res)=> {
+    res.json(req.body);
+})
 
 module.exports = router;
